bf7e6f1f48e7e221de4f15813996c88e850f22e8
Vinicius Santos <c.viniciussantos@outlook.com>
2020-07-30 13:56:57 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cvinicius/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cvinicius.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cvinicius.workshopmongo.domain.Post;
+import com.cvinicius.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
